e140e4b3b2601154301f86a62cf335133ce5d455
Adrianoprovenzano <60528407+Adrianoprovenzano@users.noreply.github.com>
2020-02-02 00:51:57 +0100

    Add files via upload

---
+++ verify.php
@@ -0,0 +1,40 @@
+
+
+
+<?php 
+session_start();
+
+$utente= $_POST['username'];
+$password= $_POST['password'];
+
+/****** Utenti che hanno accesso al Login ******/
+/***********************************************/
+
+$nome_utente_1= 'Adriano';
+$password_utente_1= 'anno.2019';
+
+/***********************************************/
+// Inizio IF statement
+/***********************************************/
+
+
+if($utente != $nome_utente_1 && $password != $password_utente_1 ) {
+	header("Location: http://localhost:8888/form_exemple/index.html");
+}elseif($utente == '' && $password == ''  ){
+	header("Location: http://localhost:8888/form_exemple/index.html");
+}elseif($utente == $nome_utente_1 && $password == $password_utente_1){
+	// potrei reindirizzare l utente verso un altra pagina php
+	echo '<h1>Welcome'. ' '. $nome_utente_1. ' Ora puoi accedere a tutte le tue foto personali!!.</h1>';
+
+}elseif($utente != $nome_utente_1 || $password != $password_utente_1 ){
+	
+	$message = "Wrong username \or password! ";
+	echo "<script type='text/javascript'>alert('$message');</script>";
+	echo '<a href="http://localhost:8888/form_exemple/">Riprova</a>';
+	
+	
+}else {
+	echo 'you are a bad hacker';
+}
+
+?>
